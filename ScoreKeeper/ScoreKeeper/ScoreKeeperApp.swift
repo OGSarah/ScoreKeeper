74e71bb1caa723c27ce0fe34959b4fbe7c597e20
Sarah Clark <sarahireneclark@gmail.com>
2025-08-20 14:51:30 -0500

    Initial commit of project

---
+++ ScoreKeeper/ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Sarah Clark on 8/20/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
