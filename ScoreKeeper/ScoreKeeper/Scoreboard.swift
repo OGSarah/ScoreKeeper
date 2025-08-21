591b8f56ee8a4c01f6a6e49896ed8d9a413010bb
Sarah Clark <sarahireneclark@gmail.com>
2025-08-20 20:13:04 -0500

    Added the Scoreboard struct and updated UI to use it

---
+++ ScoreKeeper/ScoreKeeper/Scoreboard.swift
@@ -0,0 +1,18 @@
+//
+//  Scoreboard.swift
+//  ScoreKeeper
+//
+//  Created by Sarah Clark on 8/20/25.
+//
+
+import Foundation
+
+struct Scoreboard {
+
+    var players: [Player] = [
+        Player(name: "Rachel", score: 0),
+        Player(name: "Orko", score: 0),
+        Player(name: "Molly", score: 0)
+    ]
+
+}
